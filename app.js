c556515ca355acd54d17e2c7a49db4f4a09292ab
BeToVan1 <bevanto49797@gmail.com>
2023-09-16 16:52:28 -0700

    added google oauth and sessions

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const path = require("path");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -14,42 +18,56 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//connects to db
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
 
+
+//schema for user
 const userSchema = new mongoose.Schema ({
     email: String,
-    password: String
-})
+    password: String,
+    googleId: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
 
-// const GoogleStrategy = require('passport-google-oauth20').Strategy;
-// const findOrCreate = require('mongoose-findorcreate');
+passport.serializeUser(function(user,done){
+    done(null,user);
+});
 
-// passport.use(new GoogleStrategy({
-//     clientID: process.env.CLIENT_ID,
-//     clientSecret: process.env.CLIENT_SECRET,
-//     callbackURL: "http://localhost:3000/auth/google/MyCineList",
-//     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
-//   },
-//   function(accessToken, refreshToken, profile, cb) {
-//     User.findOrCreate({ googleId: profile.id }, function (err, user) {
-//       return cb(err, user);
-//     });
-//   }
-// ));
+passport.deserializeUser(function(user,done){
+    done(null,user);
+});
 
-// const userSchema = new mongoose.Schema ({
-//     email: String,
-//     password: String
-// })
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/MyCineList",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    //console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
-// userSchema.plugin(passportLocalMongoose);
-// userSchema.plugin(findOrCreate);
 
-// app.get('/auth/google',
-//   passport.authenticate('google', { scope: ['profile'] }));
 app.get('./movies.js', (req, res) => {
     res.set('Content-Type', 'application/javascript');
     // Rest of the code to send the file
@@ -59,6 +77,16 @@ app.get("/", function(req,res){
     res.render("index");
 });
 
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/MyCineList', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    //placeholder
+    res.redirect('/index');
+  });
+
 app.get("/login", function(req,res){
     res.render("login");
 });
@@ -67,36 +95,54 @@ app.get("/register", function(req,res){
     res.render("register");
 });
 
-app.post("/register", (req,res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-
-    newUser
-        .save()
-        .then(() =>{
-            //placeholder- should render logged in 
-            res.render("index");
-        });
+//placeholder
+app.get("/index", function(req,res){
+    if(req.isAuthenticated()){
+        res.render("index");
+    }else{
+        res.redirect("login");
+    }
 });
 
-app.post("/login", function(req,res){
-    const username = req.body.username;
-    const password = md5(req.body.password);
+//need to add logout button 
+app.get("/logout", function(req,res){
+    req.logout();
+    res.redirect("/");
+});
 
-    User.findOne({email: username}).exec()
-    .then((foundUser) => {
-        if(foundUser){
-            if(foundUser.password === password){
+//register 
+app.post("/register", (req,res) => {
+    User.register({username: req.body.username}, req.body.password, function(err,user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }else{
+            passport.authenticate("local")(req,res,function(){
                 //placeholder
-                res.render("index");
-            }
+                res.redirect("/index");
+            });
         }
-    }).catch((err) =>{
-        console.log(err);
     });
-       
+    
+});
+
+//login 
+app.post("/login", function(req,res){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+        }else{
+            passport.authenticate("local")(req,res,function(){
+                //placeholder
+                res.redirect("/index");
+            });
+        }
+    });
 });
 
 app.listen(3000,function(){
